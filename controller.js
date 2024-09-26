b347f195867cc56bd6b68a41e03aa5449c56f90b
Jon <lundbyjon@hotmail.com>
2024-09-27 01:55:45 +0200

    creating the grid with control panel

--- controller.js
+++ controller.js
@@ -1,10 +1,28 @@
-import * as model from "./model.js";
+import Grid from "./model.js";
 import * as view from "./view.js";
+
+let GRID_HEIGHT = 10;
+let GRID_WIDTH = 10;
 
 init();
 
 function init() {
     console.log("Controller initialized");
-    model.init();
     view.init();
+
+    document.querySelector("#start-btn").addEventListener("click", startGame);
+    
 }
+
+function startGame() {
+    GRID_HEIGHT = parseInt(document.querySelector("#row-size-input").value);
+    GRID_WIDTH = parseInt(document.querySelector("#column-size-input").value);
+
+    console.log(`Game starting!\ngrid height: ${GRID_HEIGHT}\ngrid width: ${GRID_WIDTH}`);
+    
+    const model = new Grid(GRID_HEIGHT, GRID_WIDTH, 1);
+    console.table(model.grid)
+    
+    view.createVisulGrid(model)
+}
+
--- view.js
+++ view.js
@@ -1,7 +1,25 @@
 import * as controller from "./controller.js";
 
-export { init };
+export { init, createVisulGrid };
 
 function init() {
     console.log("View initialized");
+}
+
+function createVisulGrid(model) {
+    const visualGrid = document.querySelector("#grid-container");
+    visualGrid.innerHTML = "";
+
+    // opdatere css #grid-container template rows/cols
+    document.documentElement.style.setProperty("--grid-rows", model.rows);
+    document.documentElement.style.setProperty("--grid-cols", model.cols);
+
+    for (let row = 0; row < model.rows; row++) {
+        console.log("new row");
+        for (let col = 0; col < model.cols; col++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            visualGrid.insertAdjacentElement("beforeend", cell);
+        }
+    }
 }
